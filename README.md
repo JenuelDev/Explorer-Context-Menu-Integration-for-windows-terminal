3a86962729b97d2027c013bf1a5c89bc3bb69323
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2020-09-11 23:48:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Explorer-Context-Menu-Integration-for-windows-terminal
+ This is a bat file that would easily register the context menu for the windows terminal
---
+++ Uninstall.bat
@@ -0,0 +1,3 @@
+reg delete "HKCU\Software\Classes\Directory\shell\Open Windows Terminal here" /f
+reg delete "HKCU\Software\Classes\Directory\Background\shell\Open Windows Terminal here" /f
+reg delete "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Windows Terminal here" /f
